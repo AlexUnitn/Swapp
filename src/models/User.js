bc92aa9aaa2c18e8c38edae67e82219a0a3e9b38
AlexUnitn <alex.zecchin@studenti.unitn.it>
2025-12-10 11:30:14 +0100

    crea schema User

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,50 @@
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema({})
+const userSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true, lowercase: true },
+  password: { type: String, required: true }, 
+  phoneNumber: { type: String, required: true, unique: true },
+  
+  
+  address: {
+    street: String,
+    city: String,
+    postalCode: String,
+  },
 
-module.exports = mongoose.models('User', UserSchema)
+  role: { 
+    type: String, 
+    enum: ['user', 'admin'], 
+    default: 'user' 
+  },
+  
+  accountStatus: { 
+    type: String, 
+    enum: ['active', 'suspended', 'banned'], 
+    default: 'active' 
+  },
+  
+  suspensionEndDate: Date,
+  
+  // Array di oggetti
+  penalties: [{
+    type: { type: String }, 
+    reason: String,
+    appliedAt: Date,
+    expiresAt: Date
+  }],
+  
+  statistics: {
+    objectsPublished: { type: Number, default: 0 },
+    objectsBorrowed: { type: Number, default: 0 },
+    objectsLent: { type: Number, default: 0 },
+    completedTransactions: { type: Number, default: 0 }
+  },
+  
+  lastLoginAt: Date
+
+}, { timestamps: true }) 
+
+const User = mongoose.model('User', userSchema)
