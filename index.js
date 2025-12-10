de749a184465bc1158caf445f1ba8f56aba860be
AlexUnitn <alex.zecchin@studenti.unitn.it>
2025-12-10 15:42:15 +0100

    ristruttura server

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const express = require("express");
-const app = express();
+//Caricamento variabili d'ambiente 
+require('dotenv').config()
+const express = require('express')
+const userRouter = require('./routes/userRoutes')
+const connectDB = require('./db')
 
-app.get("/", (req, res) => {
-  res.send("Server running correctly!");
-});
+app.use('/api/users', userRouter)
 
+const app = express()
+connectDB()
 app.listen(3000, () => {
-  console.log("Server running on port 3000");
-});
+    console.log('Server in ascolto sulla porta 3000')
+})
