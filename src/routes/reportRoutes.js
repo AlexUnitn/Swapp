da530b3fc6d23ec3ff8dc11f49febffa46353a63
AlexUnitn <alex.zecchin@studenti.unitn.it>
2025-12-11 14:28:23 +0100

    corregge il percorso del controller

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const itemController = require('../controllers/itemControllers')
+const itemController = require('../controllers/reportControllers')
 
 // GET api/report
 router.get('/', itemController.getReport)
