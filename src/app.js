5916439005755a73af9e62b1a23a05f60779e1df
AlexUnitn <alex.zecchin@studenti.unitn.it>
2026-02-03 19:13:13 +0100

    abilita CORS, ridirotta nel main gestione dei fallback

--- src/app.js
+++ src/app.js
@@ -6,19 +6,28 @@ const itemRouter = require('./routes/itemRoutes')
 const reportRouter = require('./routes/reportRoutes')
 const bookingRouter = require('./routes/bookingRoutes')
 const authRouter = require('./routes/authRoutes')
+const cors = require('cors')
+const path = require('path')
 
 const connectDB = require('./db')
 
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 connectDB()
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
 app.use('/api/item', itemRouter)
 app.use('/api/report', reportRouter)
 app.use('/api/booking', bookingRouter)
+
+app.use(express.static(path.join(__dirname, '../frontend/main')))
+
+// rotta di fallback per tutte le altre richieste
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/errore/404.html'))
+})
 
 app.listen(3000, () => {
     console.log('Server in ascolto sulla porta 3000')
