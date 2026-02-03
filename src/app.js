2554d11f75d62def9edcb2505fa2799c32893c95
AlexUnitn <alex.zecchin@studenti.unitn.it>
2026-02-03 18:56:08 +0100

    integra il frontend con il backend per la ricerca dinamica dei risultati.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 //Caricamento variabili d'ambiente 
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
+const cors = require('cors') 
 const userRouter = require('./routes/userRoutes')
 const itemRouter = require('./routes/itemRoutes')
 const reportRouter = require('./routes/reportRoutes')
@@ -12,13 +14,24 @@ const connectDB = require('./db')
 
 const app = express()
 app.use(express.json())
+app.use(cors()) // Abilita CORS
 
 connectDB()
+
+// API Routes
 app.use('/api/auth', authRouter)
 app.use('/api/users', userRouter)
 app.use('/api/item', itemRouter)
 app.use('/api/report', reportRouter)
 app.use('/api/booking', bookingRouter)
+
+// Servire i file statici del Frontend
+app.use(express.static(path.join(__dirname, '../frontend/main')))
+
+// Rotta di fallback: per qualsiasi altra richiesta, restituisci la homepage
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/main/index.html'))
+})
 
 app.listen(3000, () => {
     console.log('Server in ascolto sulla porta 3000')
