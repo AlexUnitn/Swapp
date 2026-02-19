ab95c73382941c57e813601991ba98ef87f1efb6
AlexUnitn <alex.zecchin@studenti.unitn.it>
2026-02-19 17:54:19 +0100

    aggiunge il middleware alla route user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
+const auth = require('../middleware/authMiddleware')
 
 // GET api/users
-router.get('/', userController.getUser)
+router.get('/', auth, userController.getUser)
 // PUT api/users/:id
-router.put('/:id', userController.updateUser)
+router.put('/:id', auth, userController.updateUser)
 // GET api/users/:id
-router.get('/:id', userController.getUserById)
+router.get('/:id', auth, userController.getUserById)
 // DELETE api/users/:id
-router.delete('/:id', userController.deleteUser)
+router.delete('/:id', auth, userController.deleteUser)
 
 module.exports = router
