d5093aededcfc0823dec150b14c7a0ad3c54ed33
AlexUnitn <alex.zecchin@studenti.unitn.it>
2026-02-19 17:54:02 +0100

    aggiunge il middleware alla route message

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const messageController = require('../controllers/messageControllers')
+const auth = require('../middleware/authMiddleware')
 
 // GET /api/messages/conversation/:key
-router.get('/conversation/:key', messageController.getConversation)
+router.get('/conversation/:key', auth, messageController.getConversation)
 
 // POST /api/messages
-router.post('/', messageController.createMessage)
+router.post('/', auth, messageController.createMessage)
 
 module.exports = router
