84969d317bf486b867e26c98a75872ec3214b9c8
AlexUnitn <alex.zecchin@studenti.unitn.it>
2026-02-19 17:53:52 +0100

    aggiunge il middleware alla route item

--- src/routes/itemRoutes.js
+++ src/routes/itemRoutes.js
@@ -1,19 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const itemController = require('../controllers/itemControllers')
+const auth = require('../middleware/authMiddleware')
 
 // GET api/item
 router.get('/', itemController.getItem)
 // POST api/item
-router.post('/', itemController.createItem)
+router.post('/', auth, itemController.createItem)
 // PUT api/item/:id
-router.put('/:id', itemController.updateItem)
+router.put('/:id', auth, itemController.updateItem)
 // GET api/item/:id
 router.get('/:id', itemController.getItemById)
 // DELETE api/item/:id
-router.delete('/:id', itemController.deleteItem)
+router.delete('/:id', auth, itemController.deleteItem)
 // GET api/item/user/:userId
 router.get('/user/:userId', itemController.getItemsByUserId)
-
 
 module.exports = router
