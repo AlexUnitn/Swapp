ab1435a01ed973cccda925d5ce62fac365631d45
AlexUnitn <alex.zecchin@studenti.unitn.it>
2026-02-19 17:54:11 +0100

    aggiunge il middleware alla route report

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const reportController = require('../controllers/reportControllers')
+const auth = require('../middleware/authMiddleware')
 
 // GET api/report
-router.get('/', reportController.getReport)
+router.get('/', auth, reportController.getReport)
 // POST api/report
-router.post('/', reportController.createReport)
+router.post('/', auth, reportController.createReport)
 // GET api/report/:id
-router.get('/:id', reportController.getReportById)
+router.get('/:id', auth, reportController.getReportById)
 module.exports = router
