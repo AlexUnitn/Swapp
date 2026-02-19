6ef0696c527226ea5b292f51fc7279cacb8010d1
AlexUnitn <alex.zecchin@studenti.unitn.it>
2026-02-19 17:53:39 +0100

    aggiunge il middleware alla route booking

--- src/routes/bookingRoutes.js
+++ src/routes/bookingRoutes.js
@@ -1,18 +1,19 @@
 const express = require('express')
 const router = express.Router()
 const bookingController = require('../controllers/bookingControllers')
+const auth = require('../middleware/authMiddleware')
 
 // GET api/booking
-router.get('/', bookingController.getBookings)
+router.get('/', auth, bookingController.getBookings)
 // POST api/booking
-router.post('/', bookingController.createBooking)
+router.post('/', auth, bookingController.createBooking)
 // DELETE api/booking
-router.delete('/:id', bookingController.deleteBooking)
+router.delete('/:id', auth, bookingController.deleteBooking)
 // PUT api/booking/:id
-router.put('/:id', bookingController.updateBooking)
+router.put('/:id', auth, bookingController.updateBooking)
 // GET api/booking/:id
-router.get('/:id', bookingController.getBookingById)
+router.get('/:id', auth, bookingController.getBookingById)
 // GET api/booking/user/:id
-router.get('/user/:id', bookingController.getBookingsByUserId)
+router.get('/user/:id', auth, bookingController.getBookingsByUserId)
 
 module.exports = router
